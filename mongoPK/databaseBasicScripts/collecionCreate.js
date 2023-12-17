e4fa80c7a423b394c02e4e7e6d0a0b0fbeccc7e0
BartoszGrzanka <91725411+BartoszGrzanka@users.noreply.github.com>
2023-12-17 15:00:22 +0100

    mongo server + basic scripts
    mongodb server with scripts:
    -create db and 
    -create collection in chosen db
    -test showing all databases

---
+++ mongoPK/databaseBasicScripts/collecionCreate.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb');
+
+/**
+ * Creates a new collection in the specified database.
+ * 
+ * @async
+ * @function
+ * @param {string} uri - The MongoDB connection string.
+ * @param {string} databaseName - The name of the database where the collection will be created.
+ * @param {string} collectionName - The name of the collection to create.
+ * @returns {Promise<void>} A Promise that resolves when the collection is created.
+ * @throws {Error} If there is an error during the collection creation process.
+ */
+async function createCollection(uri, databaseName, collectionName) {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+
+    // Access the specified database
+    const db = client.db(databaseName);
+
+    // Create a new collection
+    await db.createCollection(collectionName);
+
+    console.log(`Collection '${collectionName}' created in the '${databaseName}' database.`);
+  } finally {
+    await client.close();
+    console.log('Connection closed.');
+  }
+}
+
+// Example usage:
+// Replace 'mongodb://localhost:27017', 'Test', and 'MyCollection' with your desired values
+createCollection('mongodb://localhost:27017', 'Test', 'MyCollection').catch(console.error);
