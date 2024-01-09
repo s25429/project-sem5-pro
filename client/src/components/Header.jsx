b6c95fee3b5859f4fb3cfc6247e9765e389debe6
Cezary Ciślak <s25429@pjwstk.edu.pl>
2024-01-09 11:45:37 +0100

    added global styles and styled header with sliding nav

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 
+import Logo from '../assets/logo.svg'
+import { FaEllipsisVertical } from 'react-icons/fa6'
+
+import '../css/Header.css'
+
 
 function Header({ title = 'Półkarzyk' }) {
     const [toggleNav, setToggleNav] = useState(false)
 
     return (
         <header>
-            <i className='logo'></i>
+            <img className='logo' src={Logo} alt='logo' />
             <h1>{title}</h1>
-            <i onClick={() => setToggleNav(!toggleNav)}>H</i>
-            {toggleNav ? (
-                <nav>
-                    siema
-                </nav>
-            ) : null}
+            <FaEllipsisVertical onClick={() => setToggleNav(!toggleNav)} className='toggler-nav' />
+            <nav aria-hidden={!toggleNav}>
+                siema
+            </nav>
         </header>
     )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+import './css/variables.css'
 import './css/main.css'
+import './css/resets.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
