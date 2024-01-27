6cfcb7c09b1a737dd7334462a8b15df171c28f13
Cezary Ciślak <s25429@pjwstk.edu.pl>
2024-01-27 16:09:22 +0100

    added proxy to vite configuration to handle server to/from client connections

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    server: {
+        proxy: {
+            '/healthcheck': 'http://localhost:5172',
+            '/db': 'http://localhost:5172',
+        },
+    },
+    plugins: [react()],
 })
