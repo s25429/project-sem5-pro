e129a1089a15bab5dbcbc7f754a700b19bbf51e8
Cezary Ciślak <s25429@pjwstk.edu.pl>
2024-01-27 10:17:50 +0100

    added shop map generator and doing first tests

--- client/src/pages/ShopMap/ShopMap.jsx
+++ client/src/pages/ShopMap/ShopMap.jsx
@@ -1,34 +1,60 @@
-import React from 'react'
+import { useEffect } from 'react'
+
+import { config } from '../../vendor/shop-map-generator/config'
+import { createMap } from '../../vendor/shop-map-generator'
 
 import '../../css/ShopMap.css'
 
+const sampleData = {
+    id: 1,
+    name: 'Sample Shop Name',
+    location: '1.2,3.4',
+    objects: [
+        {
+            category: 'cat-1',
+            group: 'group-1',
+            x: 20,
+            y: 20,
+            width: 150,
+            height: 100,
+        }, 
+        {
+            category: 'cat-2',
+            group: 'group-1',
+            x: 50,
+            y: 80,
+            width: 60,
+            height: 200,
+        }, 
+        {
+            category: 'cat-3',
+            group: 'group-1',
+            x: 0,
+            y: 0,
+            width: 70,
+            height: 50,
+        }, 
+        {
+            category: 'cat-4',
+            group: 'group-2',
+            x: 200,
+            y: 100,
+            width: 70,
+            height: 50,
+        }
+    ]
+}
+
 
 function ShopMap() {
+    useEffect(() => {
+        config.container = '#shop-map'
+
+        createMap(sampleData)
+    }, [])
+
     return (
-        <div>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
-            <p>hello</p>
+        <div id='shop-map'>
         </div>
     )
 }
---
+++ client/src/vendor/shop-map-generator/struct.js
@@ -0,0 +1,86 @@
+export const struct = {
+    id: Number(),
+    name: String(),
+    location: String(),
+    objects: [{
+        category: String(),
+        group: String(),
+        x: Number(),
+        y: Number(),
+        width: Number(),
+        height: Number(),
+    }]
+}
+
+/**
+ * Checks format of struct
+ * @param {typeof struct} data
+ * @returns true if data has all properties with correct types, flase otherwise
+ */
+export const parseStruct = (data) => {
+    const results = {
+        id:       parse(data?.id,       struct.id,       { canBeZero: true }                      ),
+        name:     parse(data?.name,     struct.name                                               ),
+        location: parse(data?.location, struct.location, { checks: [{ fn: parseLocationString }] }),
+
+        objects: data?.objects && Array.isArray(data.objects) && data.objects.map(object => ({
+            category: parse(object?.category, struct.objects[0].category                                         ),
+            group:    parse(object?.group,    struct.objects[0].group                                            ),
+            x:        parse(object?.x,        struct.objects[0].x,       { canBeZero: true, canBeNegative: true }),
+            y:        parse(object?.y,        struct.objects[0].y,       { canBeZero: true, canBeNegative: true }),
+            width:    parse(object?.width,    struct.objects[0].width,   { canBeZero: true }                     ),
+            height:   parse(object?.height,   struct.objects[0].height,  { canBeZero: true }                     ),
+        }))
+    }
+
+    const flattenedResults = Object.values(results).map(value => Array.isArray(value) ? value.map(object => Object.values(object)) : value).flat(2)
+
+    if (flattenedResults.every(check => check === true)) 
+        return true
+
+    console.error('Parsing result:', results)
+    return false
+}
+
+/**
+ * Checks if location is valid
+ * @param {string} loc - location as latitute and longtitude string, e.g. "33.9867,-44.27198"
+ * @returns true if location is in valid format, false otherwise
+ */
+function parseLocationString(loc) {
+    const [lat, long, ...rest] = loc.split(',')
+    return !isNaN(lat) && !isNaN(long) && rest.length === 0
+}
+
+/**
+ * Parses a vlaue against multiple checks
+ * @param {any} value - param to check its type
+ * @param {any} against - param which type should be equal to value
+ * @param {{ canBeNull?: boolean, canBeUndefined?: boolean, canBeZero?: boolean, canBeNegative?: boolean, checks: Array<{ fn: (value, ...args) => boolean, args?: Array<any> }> }} arg - type hint 
+ * @param canBeNull - default=false
+ * @param canBeUndefined - default=false
+ * @param canBeZero - only for `typeof value == number`, default=false
+ * @param canBeNegative - only for `typeof value == number`, default=false
+ * @param checks - array od additional checks being functions and additional arguments to these functions
+ * @returns 
+ */
+function parse(value, against, { canBeNull = false, canBeUndefined = false, canBeZero = false, canBeNegative = false, checks = [] } = {}) {
+    if (!canBeNull && value === null)
+        return false
+
+    if (!canBeUndefined && value === undefined)
+        return false
+
+    if (!canBeZero && typeof value == 'number' && value === 0)
+        return false
+
+    if (!canBeNegative && typeof value == 'number' && value < 0)
+        return false
+
+    if (typeof value != typeof against)
+        return false
+
+    return checks.length === 0
+        ? true
+        : checks.map(({ fn, args = [] }) => fn(value, ...args)).every(check => check === true)
+}
